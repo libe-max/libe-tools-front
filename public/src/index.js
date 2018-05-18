85fd98eb1aaa60d3d611bceb8f3d18e83a867455
Maxime Fabas <m.fabas@liberation.fr>
2018-05-18 12:32:08 +0200

    Removed unnecessary files in the React app

--- public/src/index.js
+++ public/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+class App extends Component {
+  render () {
+    return <div className='app'>Hello world!</div>
+  }
+}
+
+// Render app ---------------------------------------------
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
 registerServiceWorker()
