413bbd3c91aa98c3b474c8d9b9197f6b740573ce
Maxime Fabas <m.fabas@liberation.fr>
2018-05-23 10:49:53 +0200

    Back – Updated some comments

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const router = express.Router()
 
-/* GET home page. */
+/* GET React app. */
 router.get('/', function (req, res, next) {
   res.sendFile(
     path.join(__dirname, '../public/build/index.html')
