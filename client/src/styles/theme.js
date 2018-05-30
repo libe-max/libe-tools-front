3b5fbcaa0a551e142feab09d2d410942147233b2
Maxime Fabas <m.fabas@liberation.fr>
2018-05-30 11:33:57 +0200

    Front – Cleaned up theme.js

--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -5,22 +5,26 @@ injectGlobal`@font-face {
   font-family: 'LibeSans Semi Condensed';
   src: url('/fonts/libe-sans-semi-condensed/LibeSansSemiCondensed-Bold.otf');
 }`
+
 injectGlobal`@font-face {
   font-family: 'Zurich';
   src: url('/fonts/zurich-bt/Zurich-BT.ttf');
   font-weight: 400;
 }`
+
 injectGlobal`@font-face {
   font-family: 'Zurich';
   src: url('/fonts/zurich-bt/Zurich-Italic-BT.ttf');
   font-weight: 400;
   font-style: italic;
 }`
+
 injectGlobal`@font-face {
   font-family: 'Zurich';
   src: url('/fonts/zurich-bt/Zurich-Bold-BT.ttf');
   font-weight: 600;
 }`
+
 injectGlobal`@font-face {
   font-family: 'Zurich';
   src: url('/fonts/zurich-bt/Zurich-Bold-Italic-BT.ttf');
@@ -29,15 +33,18 @@ injectGlobal`@font-face {
 }`
 
 const units = val => `${8 * val}px`
+
 const fonts = {
   brand: 'LibeSans Semi Condensed',
   main: 'Zurich'
 }
+
 const colors = {
   black: '#1B1B1F',
   grey: '#7B7B80',
   lightGrey: '#DDDDDD'
 }
+
 const shadows = {
   medium: `0 0 ${units(1)} ${units(.25)} ${hexToRgba(colors.black, .2)}`,
   small: `0 ${units(.25)} ${units(.5)} 0 ${hexToRgba(colors.black, .1)}`
