3fae56462ff75eca7a4f0bde87645bd6b363f9db
Maxime Fabas <m.fabas@liberation.fr>
2018-06-07 10:12:47 +0200

    Front – Updated components page

--- client/src/pages/ComponentsPage.js
+++ client/src/pages/ComponentsPage.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import MainTitle from '../components/MainTitle'
 import BlockTitle from '../components/BlockTitle'
 import Button from '../components/Button'
+import TextInput from '../components/TextInput'
+import FileInput from '../components/FileInput'
 import Header from '../components/Header'
 import LibeToolThumb from '../components/LibeToolThumb'
 import LibeBundleThumb from '../components/LibeBundleThumb'
@@ -29,7 +31,11 @@ const CpRow = ({title, children}) => {
   `
   return (
     <CpRow>
-      {title ? <div id='title'>{title}</div> : null}
+      <div
+        id='title'
+        style={{display: title ? 'block' : 'none'}}>
+        {title}
+      </div>
       <div id='content'>{children}</div>
     </CpRow>
   )
@@ -46,8 +52,11 @@ const CpBx = ({title, width, children}) => {
     > #title {
       font-family: ${props => props.theme.fonts.main};
       color: ${props => props.theme.colors.lightText};
-      margin-bottom: ${props => props.theme.units(1)};
+      margin-bottom: ${props => props.theme.units(2)};
       width: 100%;
+    }
+    > div > * {
+      margin-bottom: ${props => props.theme.units(2)};
     }
   `
   return (
@@ -81,12 +90,50 @@ export default class ComponentsPage extends Component {
           <CpBx title='Button link dangerous' width={1/4}><Button link dangerous>Click me!</Button></CpBx>
           <CpBx title='Button link minor dangerous' width={1/4}><Button link minor dangerous>Click me!</Button></CpBx>
         </CpRow>
+        <CpRow title='Inputs'>
+          <CpBx title='Text input with label' width={1/4}>
+            <TextInput
+              label='Label'
+              placeholder='Placeholder' />
+          </CpBx>
+          <CpBx title='File input with label' width={1/4}>
+            <FileInput
+              label='Label'
+              placeholder='Choisir un fichier' />
+          </CpBx>
+          <CpBx title='File input with label' width={1/4}>
+            <FileInput
+              label='Label'
+              placeholder='Choisir un fichier'
+              src='https://i2.wp.com/www.shannakress.com/wp-content/uploads/image207.jpg' />
+          </CpBx>
+          <CpBx title='File input with label' width={1/4}>
+            <FileInput
+              label='Label'
+              placeholder='Choisir un fichier'
+              src='http://maximefabas.github.io/ressources/maxime_fabas_resume_fr_feb_18.pdf' />
+          </CpBx>
+        </CpRow>
         <CpRow title='Compound components'>
           <CpBx title='Header'><Header /></CpBx>
-          <CpBx title='Libé tool thumb' width={1/2}><LibeToolThumb /></CpBx>
-          <CpBx title='Libé bundle thumb' width={1/2}>
+          <CpBx title='Libé tool thumb' width={1/3}><LibeToolThumb /></CpBx>
+          <CpBx title='Libé bundle thumb' width={1/3}>
             <LibeBundleThumb loading />
-            <LibeBundleThumb />
+            <LibeBundleThumb
+              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
+              type='Témoignage'
+              author='Aurélie Delmas'
+              created={1521590400000}
+              updated={1521676800000}
+              image='http://images.freeimages.com/images/previews/250/some-image-1302910.jpg'/>
+            <LibeBundleThumb
+              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
+              type='Témoignage'
+              author='Aurélie Delmas'
+              created={1521590400000}
+              updated={1521676800000}
+              published={1521776800000}
+              image='http://images.freeimages.com/images/previews/250/some-image-1302910.jpg'/>
           </CpBx>
         </CpRow>
       </ComponentsPage>
