358faaa8b71c33057d9f8edac5d45b874a333ef7
Maxime Fabas <m.fabas@liberation.fr>
2018-06-08 10:27:02 +0200

    Front – Button component – Some cleaning in code

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -5,75 +5,69 @@ export default class Button extends Component {
   render () {
     const props = this.props
     const Button = styled.button`
-      font-family: ${props => props.theme.fonts.main};
-      font-size: ${props => props.theme.units(2)};
-      line-height: ${props => props.theme.units(3)};
+      font-family: ${p => p.theme.fonts.main};
+      font-size: ${p => p.theme.units(2)};
+      line-height: ${p => p.theme.units(3)};
       font-style: ${props.dangerous ? 'italic' : null};
       font-weight: 400;
-      height: ${props => props.theme.units(4)};
-      background: ${props => {
-        if (props.minor && props.dangerous) return props.theme.colors.shadedBg
-        else if (props.dangerous) return props.theme.colors.dangerText
-        else if (props.minor) return props.theme.colors.shadedBg
-        else return props.theme.colors.action
+      height: ${p => p.theme.units(4)};
+      background: ${p => {
+        if (props.minor && props.dangerous) return p.theme.colors.shadedBg
+        else if (props.dangerous) return p.theme.colors.dangerText
+        else if (props.minor) return p.theme.colors.shadedBg
+        else return p.theme.colors.action
       }};
-      color: ${props => {
-        if (props.minor && props.dangerous) return props.theme.colors.dangerText
-        else if (props.dangerous) return props.theme.colors.clearText
-        else if (props.minor) return props.theme.colors.lightText
-        else return props.theme.colors.clearText
+      color: ${p => {
+        if (props.minor && props.dangerous) return p.theme.colors.dangerText
+        else if (props.dangerous) return p.theme.colors.clearText
+        else if (props.minor) return p.theme.colors.lightText
+        else return p.theme.colors.clearText
       }}
       padding:
-        ${props => props.theme.units(.5)}
-        ${props => props.theme.units(2)};
-      border-radius: ${props => props.theme.units(2)};
-      box-shadow: ${props => props.minor ? null : props.theme.shadows.neat};
+        ${p => p.theme.units(.5)}
+        ${p => p.theme.units(2)};
+      border-radius: ${p => p.theme.units(2)};
+      box-shadow: ${p => props.minor ? null : p.theme.shadows.neat};
       cursor: pointer;
       border: none;
       box-sizing: border-box;
       :hover {
         box-shadow: 
-          ${props => props.theme.shadows.neat},
-          ${props => props.theme.shadows.small};
+          ${p => p.theme.shadows.neat},
+          ${p => p.theme.shadows.small};
       }
     `
     const PrimaryButton = Button.extend`
-      font-size: ${props => props.theme.units(2.5)};
+      font-size: ${p => p.theme.units(2.5)};
       font-weight: 600;
-      line-height: ${props => props.theme.units(3)};
-      border-radius: ${props => props.theme.units(3)};
-      height: ${props => props.theme.units(6)};
+      line-height: ${p => p.theme.units(3)};
+      border-radius: ${p => p.theme.units(3)};
+      height: ${p => p.theme.units(6)};
       padding:
-        ${props => props.theme.units(1.5)}
-        ${props => props.theme.units(3)};
+        ${p => p.theme.units(1.5)}
+        ${p => p.theme.units(3)};
     `
     const LinkButton = Button.extend`
-      color: ${props => {
-        if (props.dangerous) return props.theme.colors.dangerText
-        else if (props.minor) return props.theme.colors.lightText
-        else return props.theme.colors.action
+      color: ${p => {
+        if (props.dangerous) return p.theme.colors.dangerText
+        else if (props.minor) return p.theme.colors.lightText
+        else return p.theme.colors.action
       }};
       padding: 0;
       box-shadow: none;
       text-decoration: underline;
       height: auto;
       background: transparent;
-      :hover {
-        box-shadow: none;
-      }
+      :hover { box-shadow: none; }
     `
-    if (props.primary) return (
-      <PrimaryButton {...props}>
-        {props.children}
-      </PrimaryButton>)
-    else if (props.link) return (
-      <LinkButton {...props}>
-        {props.children}
-      </LinkButton>)
-    else return (
-      <Button {...props}>
-        {props.children}
-      </Button>
-    )
+    if (props.primary) return <PrimaryButton {...props}>
+      {props.children}
+    </PrimaryButton>
+    else if (props.link) return <LinkButton {...props}>
+      {props.children}
+    </LinkButton>
+    else return <Button {...props}>
+      {props.children}
+    </Button>
   }
 }
