767b2b52d6b32986aa39c0f83f3fdb56e8e479a3
Maxime Fabas <m.fabas@liberation.fr>
2018-06-08 12:47:01 +0200

    Front – Paragraph component – Added light and small variants

--- client/src/components/Paragraph.js
+++ client/src/components/Paragraph.js
@@ -4,17 +4,16 @@ import styled from 'styled-components'
 export default class Paragraph extends Component {
   render () {
     const props = this.props
-    const Paragraph = styled.div`
-      > p {
-        margin: 0;
-        padding: 0;
-        font-family: ${p => p.theme.fonts.main};
-        font-size: ${p => p.theme.units(2)};
-        line-height: ${p => p.theme.units(3)};
-      }
+    const Paragraph = styled.p`
+      margin: 0;
+      padding: 0;
+      font-family: ${p => p.theme.fonts.main};
+      line-height: ${p => props.small ? p.theme.units(2) : p.theme.units(3)};
+      font-size: ${p => props.small ? p.theme.units(1.75) : p.theme.units(2)};
+      color: ${p => props.light ? p.theme.colors.lightText : p.theme.colors.text};
     `
     return <Paragraph {...props}>
-      <p>{props.children}</p>
+      {props.children}
     </Paragraph>
   }
 }
