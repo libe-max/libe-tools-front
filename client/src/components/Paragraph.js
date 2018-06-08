cc3e54c399251fdb1e1b34c5b382a946e1867a61
Maxime Fabas <m.fabas@liberation.fr>
2018-06-08 11:38:27 +0200

    Front – Paragraph component – Some cleaning in code

--- client/src/components/Paragraph.js
+++ client/src/components/Paragraph.js
@@ -8,15 +8,13 @@ export default class Paragraph extends Component {
       > p {
         margin: 0;
         padding: 0;
-        font-family: ${props => props.theme.fonts.main};
-        font-size: ${props => props.theme.units(2)};
-        line-height: ${props => props.theme.units(3)};
+        font-family: ${p => p.theme.fonts.main};
+        font-size: ${p => p.theme.units(2)};
+        line-height: ${p => p.theme.units(3)};
       }
     `
-    return (
-      <Paragraph {...props}>
-        <p>{props.children}</p>
-      </Paragraph>
-    )
+    return <Paragraph {...props}>
+      <p>{props.children}</p>
+    </Paragraph>
   }
 }
