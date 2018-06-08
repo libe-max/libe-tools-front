0543da192992831ce2b63f09317cf776dec6ad19
Maxime Fabas <m.fabas@liberation.fr>
2018-06-08 13:10:52 +0200

    Front – MainTitle component – Now uses Heading component

--- client/src/components/MainTitle.js
+++ client/src/components/MainTitle.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import Heading from './Heading'
 
 export default class MainTitle extends Component {
   render () {
     const props = this.props
-    const MainTitle = styled.h1`
-      font-size: ${p => p.theme.units(3)};
-      line-height: ${p => p.theme.units(4)};
-      font-family: ${p => p.theme.fonts.brand};
-      color: ${p => p.theme.colors.text};
+    const MainTitle = styled.div`
+      h1, h2, h3, h4, h5, h6, span {
+        font-size: ${p => p.theme.units(3)};
+        line-height: ${p => p.theme.units(4)};
+        font-family: ${p => p.theme.fonts.brand};
+        color: ${p => p.theme.colors.text};
+        font-weight: 600;
+        margin: 0;
+        padding: 0
+      }
     `
     return <MainTitle {...props}>
-      {props.children}
+      <Heading level={props.level || 1}>
+        {props.children}
+      </Heading>
     </MainTitle>
   }
 }
