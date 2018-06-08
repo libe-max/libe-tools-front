42316706e03741c77f9a96a1ca28855c478b6f5d
Maxime Fabas <m.fabas@liberation.fr>
2018-06-08 11:39:28 +0200

    Front – ShadowBox component – Some cleaning in code

--- client/src/components/ShadowBox.js
+++ client/src/components/ShadowBox.js
@@ -5,19 +5,17 @@ export default class ShadowBox extends Component {
   render () {
     const props = this.props
     const ShadowBox = styled.div`
-      border-radius: ${props => props.theme.units(1)};
-      box-shadow: ${props => props.theme.shadows.small};
+      border-radius: ${p => p.theme.units(1)};
+      box-shadow: ${p => p.theme.shadows.small};
       box-sizing: border-box;
       overflow: hidden;
       border:
-        ${props => props.theme.units(.25)}
-        ${props => props.theme.colors.borders}
+        ${p => p.theme.units(.25)}
+        ${p => p.theme.colors.borders}
         solid;
     `
-    return (
-      <ShadowBox {...props}>
-        {props.children}
-      </ShadowBox>
-    )
+    return <ShadowBox {...props}>
+      {props.children}
+    </ShadowBox>
   }
 }
