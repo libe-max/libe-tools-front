b53e77882053741dd64a8dc6c3af5287c6546f6f
Maxime Fabas <m.fabas@liberation.fr>
2018-06-08 13:11:00 +0200

    Front – BlockTitle component – Now uses Heading component

--- client/src/components/BlockTitle.js
+++ client/src/components/BlockTitle.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import Heading from './Heading'
 
 export default class BlockTitle extends Component {
   render () {
     const props = this.props
     const BlockTitle = styled.div`
-      font-size: ${p => p.theme.units(2.5)};
-      line-height: ${p => p.theme.units(3)};
-      font-family: ${p => p.theme.fonts.main};
-      color: ${p => p.theme.colors.text};
-      font-weight: 600;
+      h1, h2, h3, h4, h5, h6, span {
+        font-size: ${p => p.theme.units(2.5)};
+        line-height: ${p => p.theme.units(3)};
+        font-family: ${p => p.theme.fonts.main};
+        color: ${p => p.theme.colors.text};
+        font-weight: 600;
+        margin: 0;
+        padding: 0;
+      }
     `
     return <BlockTitle {...props}>
-      {props.children}
+      <Heading level={props.level || 3}>
+        {props.children}
+      </Heading>
     </BlockTitle>
   }
 }
