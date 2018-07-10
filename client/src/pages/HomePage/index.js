26ff194b2e66a5b045309c41f606fef293897e13
Maxime Fabas <m.fabas@liberation.fr>
2018-07-10 18:42:04 +0200

    Front – Home page – Re-styling this page [WIP]

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -8,101 +8,34 @@ import Wrapper from './style'
 
 export default class HomePage extends Component {
   render () {
-    return <Wrapper>
-      <Header />
-      <div className='home-content'>
-        <div className='searchable-list' id='tools'>
-          <SearchField placeholder='Rechercher un outil' />
-          <div id='tools-list'>
-            <LibeToolThumb
-              title='Libé box'
-              image='/images/libe-tool-thumb-image.jpg'
-              description='Lorem ipsum dolor sit amet consectutor' />
-            <LibeToolThumb
-              title='Libé box'
-              image='/images/libe-tool-thumb-image.jpg'
-              description='Lorem ipsum dolor sit amet consectutor' />
-            <LibeToolThumb
-              title='Libé box'
-              image='/images/libe-tool-thumb-image.jpg'
-              description='Lorem ipsum dolor sit amet consectutor' />
-            <LibeToolThumb
-              title='Libé box'
-              image='/images/libe-tool-thumb-image.jpg'
-              description='Lorem ipsum dolor sit amet consectutor' />
-            <LibeToolThumb
-              title='Libé box'
-              image='/images/libe-tool-thumb-image.jpg'
-              description='Lorem ipsum dolor sit amet consectutor' />
+    return <Wrapper className='home-page'>
+      <div className='home-page__header'><Header /></div>
+      <div className='home-page__content'>
+        <div className='home-page__tools-panel'>
+          <div className='searchable-list'>
+            <SearchField placeholder='Rechercher un outil' />
+            <div className='home-page__tools-list'>
+              <LibeToolThumb title='Libé box' image='/images/libe-tool-thumb-image.jpg' description='Lorem ipsum dolor sit amet consectutor' />
+              <LibeToolThumb title='Libé box' image='/images/libe-tool-thumb-image.jpg' description='Lorem ipsum dolor sit amet consectutor' />
+              <LibeToolThumb title='Libé box' image='/images/libe-tool-thumb-image.jpg' description='Lorem ipsum dolor sit amet consectutor' />
+              <LibeToolThumb title='Libé box' image='/images/libe-tool-thumb-image.jpg' description='Lorem ipsum dolor sit amet consectutor' />
+              <LibeToolThumb title='Libé box' image='/images/libe-tool-thumb-image.jpg' description='Lorem ipsum dolor sit amet consectutor' />
+            </div>
           </div>
         </div>
-        <div className='searchable-list' id='bundles'>
-          <SearchField placeholder='Rechercher un module' />
-          <div id='bundles-list'>
-            <LibeBundleThumb
-              image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg'
-              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
-              type='some-type'
-              author='Savinien de Rivet'
-              created={1000000000000}
-              updated={1020000000000}
-              published={1040000000000} />
-            <LibeBundleThumb
-              image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg'
-              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
-              type='some-type'
-              author='Savinien de Rivet'
-              created={1000000000000}
-              updated={1020000000000}
-              published={1040000000000} />
-            <LibeBundleThumb
-              image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg'
-              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
-              type='some-type'
-              author='Savinien de Rivet'
-              created={1000000000000}
-              updated={1020000000000}
-              published={1040000000000} />
-            <LibeBundleThumb
-              image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg'
-              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
-              type='some-type'
-              author='Savinien de Rivet'
-              created={1000000000000}
-              updated={1020000000000}
-              published={1040000000000} />
-            <LibeBundleThumb
-              image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg'
-              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
-              type='some-type'
-              author='Savinien de Rivet'
-              created={1000000000000}
-              updated={1020000000000}
-              published={1040000000000} />
-            <LibeBundleThumb
-              image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg'
-              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
-              type='some-type'
-              author='Savinien de Rivet'
-              created={1000000000000}
-              updated={1020000000000}
-              published={1040000000000} />
-            <LibeBundleThumb
-              image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg'
-              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
-              type='some-type'
-              author='Savinien de Rivet'
-              created={1000000000000}
-              updated={1020000000000}
-              published={1040000000000} />
-            <LibeBundleThumb
-              image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg'
-              title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy'
-              type='some-type'
-              author='Savinien de Rivet'
-              created={1000000000000}
-              updated={1020000000000}
-              published={1040000000000} />
+        <div className='home-page__bundles-panel'>
+          <div className='searchable-list'>
+            <SearchField placeholder='Rechercher un module' />
+            <div className='home-page__bundles-list'>
+              <LibeBundleThumb image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg' title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy' type='some-type' author='Savinien de Rivet' created={1000000000000} updated={1020000000000} published={1040000000000} />
+              <LibeBundleThumb image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg' title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy' type='some-type' author='Savinien de Rivet' created={1000000000000} updated={1020000000000} published={1040000000000} />
+              <LibeBundleThumb image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg' title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy' type='some-type' author='Savinien de Rivet' created={1000000000000} updated={1020000000000} published={1040000000000} />
+              <LibeBundleThumb image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg' title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy' type='some-type' author='Savinien de Rivet' created={1000000000000} updated={1020000000000} published={1040000000000} />
+              <LibeBundleThumb image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg' title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy' type='some-type' author='Savinien de Rivet' created={1000000000000} updated={1020000000000} published={1040000000000} />
+              <LibeBundleThumb image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg' title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy' type='some-type' author='Savinien de Rivet' created={1000000000000} updated={1020000000000} published={1040000000000} />
+              <LibeBundleThumb image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg' title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy' type='some-type' author='Savinien de Rivet' created={1000000000000} updated={1020000000000} published={1040000000000} />
+              <LibeBundleThumb image='https://upload.wikimedia.org/wikipedia/commons/d/d7/F-15C_carrying_AIM-9X_maneuvers_into_a_vertical_climb.jpg' title='Financements libyens : ces témoignages qui pointent la campagne Sarkozy' type='some-type' author='Savinien de Rivet' created={1000000000000} updated={1020000000000} published={1040000000000} />
+            </div>
           </div>
         </div>
       </div>
--- client/src/pages/HomePage/style.js
+++ client/src/pages/HomePage/style.js
@@ -2,6 +2,98 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
 
+display: grid;
+min-height: 100vh;
+max-height: 100vh;
+grid-template-rows: auto 1fr;
+
+.home-page__header {
+  grid-row: 1;
+}
+
+.home-page__content {
+  display: flex;
+  grid-row: 2;
+  min-height: 100%;
+  max-height: 100%;
+  max-width: ${p => p.theme.units(200)};
+  padding-top: ${p => p.theme.units(5)};
+  padding-right: ${p => p.theme.units(2)};
+}
+
+.home-page__tools-panel,
+.home-page__bundles-panel {
+  position: relative;
+  min-height: 100%;
+  max-height: 100%;
+  overflow-y: scroll;
+  overflow-scrolling: touch;
+  padding: 0 ${p =>p.theme.units(2)};
+  border-right-style: solid;
+  border-right:
+    ${p => p.theme.units(0.25)}
+    ${p => p.theme.colors.borders}
+    solid;
+}
+
+.home-page__tools-panel::-webkit-scrollbar,
+.home-page__bundles-panel::-webkit-scrollbar {
+  display: none;
+}
+
+.home-page__tools-panel {
+  width: 65%;
+}
+
+.home-page__bundles-panel {
+  width: 35%;
+}
+
+@media screen and (max-width: ${p => p.theme.breakpoints.wide}) {
+  .home-page__tools-panel,
+  .home-page__bundles-panel {
+    width: 50%;
+  }
+}
+
+@media screen and (max-width: ${p => p.theme.breakpoints.medium}) {
+  .home-page__tools-panel,
+  .home-page__bundles-panel {
+    width: 100%;
+  }
+}
+
+.home-page__tools-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.libe-tool-thumb {
+  flex-grow: 0;
+  flex-shrink: 0;
+  width: calc(50% - ${p => p.theme.units(1)});
+  margin-bottom: ${p => p.theme.units(2)};
+}
+
+.libe-tool-thumb:nth-child(odd) {
+  margin-right: ${p => p.theme.units(2)};
+}
+
+@media screen and (max-width: ${p => p.theme.breakpoints.wide}) {
+  .libe-tool-thumb {
+    width: 100%;
+    margin-right: 0;
+  }
+}
+
+.libe-bundle-thumb {
+  margin-bottom: ${p => p.theme.units(2)};
+}
+
+
+
+
+/*
 .header {
   position: fixed;
   top: 0;
@@ -9,7 +101,7 @@ const Wrapper = styled.div`
   z-index: 2;
 }
 
-.home-content {
+.home-page__content {
   display: flex;
   z-index: 1;
   margin-top: ${p => p.theme.units(16)};
@@ -76,6 +168,7 @@ const Wrapper = styled.div`
     margin-bottom: ${p => p.theme.units(3)};
   }
 }
+*/
 `
 
 export default Wrapper
