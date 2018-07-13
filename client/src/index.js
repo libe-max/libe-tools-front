dbc791ffce30bc5f5bea760f02719e72808e5432
Maxime Fabas <m.fabas@liberation.fr>
2018-07-13 16:34:49 +0200

    Front – Index – Merged App.js into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,18 @@ import { render } from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 // Components
 import reducer from './reducers'
 import theme from './theme'
-import App from './App'
 // Service worker
 import registerServiceWorker from './registerServiceWorker'
 
 // Create store
 const store = createStore(reducer)
+import HomePage from './containers/HomePage'
+import ComponentsPage from './pages/ComponentsPage/'
 
 // Render app ---------------------------------------------
 render(
@@ -21,6 +23,13 @@ render(
       <Router>
         <App />
       </Router>
+        <div id='app'>
+          <Switch>
+            <Route path='/'component={HomePage} exact />
+            <Route path='/components' component={ComponentsPage} exact />
+            <Route path='/' render={() => <Redirect to='/' />} />
+          </Switch>
+        </div>
     </ThemeProvider>
   </Provider>,
   document.getElementById('root')
