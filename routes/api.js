02bf89b8d999a64c256f1f01b445bc643db9b357
Maxime Fabas <m.fabas@liberation.fr>
2018-08-09 14:56:47 +0200

    Back – Router – Created a route for getting a single bundle via it's ID

--- routes/api.js
+++ routes/api.js
@@ -2,11 +2,29 @@ const express = require('express')
 const moment = require('moment')
 const router = express.Router()
 
-/* Send all bundles in database */
+/* Get all bundles from database */
 router.all('/get-all-bundles', (req, res, next) => {
   const collection = req.db.collection('bundles')
   collection.find({}, (e, docs) => {
     if (!e) res.json({err: null, data: docs})
+    else res.json({err: e, data: null})
+  })
+})
+
+/* Get one bundle by it's ID */
+router.all('/get-bundle/:id', (req, res, next) => {
+  const id = req.params.id
+  const idIsValid = id.match(/^[0-9a-fA-F]{24}$/)
+  if (!idIsValid) {
+    res.json({
+      err: `Requested bundle ID is not valid (${id})`,
+      data: null
+    })
+    return
+  }
+  const collection = req.db.collection('bundles')
+  collection.findOne({_id: id}, (e, doc) => {
+    if (!e) res.json({err: null, data: doc})
     else res.json({err: e, data: null})
   })
 })
