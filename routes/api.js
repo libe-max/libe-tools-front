a27be30b0792274b2a0aedca8a56ab1af00a94df
Maxime Fabas <m.fabas@liberation.fr>
2018-08-09 11:29:23 +0200

    Back – Router – Cheks if bundle type is valid before creating it

--- routes/api.js
+++ routes/api.js
@@ -14,18 +14,26 @@ router.all('/get-all-bundles', (req, res, next) => {
 /* Create a new bundle in database */
 router.all('/create-bundle/:type', (req, res, next) => {
   const type = req.params.type
-  const now = moment().valueOf()
-  const newBundle = {
-    type,
-    name: null,
-    settings_history: [],
-    created_on: now
+  const allowedTypes = ['yellow-word', 'libe-box']
+  if (allowedTypes.indexOf(type) + 1) {
+    const now = moment().valueOf()
+    const newBundle = {
+      type,
+      name: null,
+      settings_history: [],
+      created_on: now
+    }
+    const collection = req.db.collection('bundles')
+    collection.insert(newBundle, (e, docs) => {
+      if (!e) res.json({err: null, data: docs})
+      else res.json({err: e, data: null})
+    })
+  } else {
+    res.json({
+      data: null,
+      err: `Not allowed to create a bundle of type ${type}`
+    })
   }
-  const collection = req.db.collection('bundles')
-  collection.insert(newBundle, (e, docs) => {
-    if (!e) res.json({err: null, data: docs})
-    else res.json({err: e, data: null})
-  })
 })
 
 /* All other requests */
