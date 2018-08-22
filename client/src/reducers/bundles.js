2b1836960ca9490bfa252664f765956e8d3507bb
Maxime Fabas <m.fabas@liberation.fr>
2018-08-22 18:40:42 +0200

    Front – Reducers – Handle bundle edition

--- client/src/reducers/bundles.js
+++ client/src/reducers/bundles.js
@@ -18,6 +18,8 @@ import {
   EDIT_BUNDLE
 } from '../actions/actionTypes'
 
+import { getBundleCurrentSettings } from '../_config/bundles'
+
 const bundles = (state = {
   list: [],
   changes: [],
@@ -96,8 +98,33 @@ const bundles = (state = {
 
     /* Edit bundle */
     case EDIT_BUNDLE:
-      console.log('CHANGE THIS BUNDLE')
-      return state
+      const { id, key, value } = action
+      if (key === 'timestamp') return state
+
+      const stored = state.list.filter(bundle => bundle._id === id)[0]
+      const storedSettings = getBundleCurrentSettings(stored)
+      const changes = state.changes.filter(bundleChanges => bundleChanges._id === id)[0] || { _id: id }
+      const newChanges = Object.assign({}, changes, { [key]: value })
+
+      if (storedSettings[key] === newChanges[key]) delete newChanges[key]
+      if (
+        Object.keys(newChanges).length <= 1 ||
+        !newChanges.hasOwnProperty('_id')
+      ) return Object.assign({}, state, {
+        changes: [...state.changes.map(bundleChanges => {
+          const id1 = bundleChanges._id
+          const id2 = id
+          return (id1 !== id2) ? bundleChanges : undefined
+        })].filter(elt => elt)
+      })
+
+      return Object.assign({}, state, {
+        changes: [...state.changes.map(bundleChanges => {
+          const id1 = bundleChanges._id
+          const id2 = id
+          return (id1 !== id2) ? bundleChanges : undefined
+        }), newChanges].filter(elt => elt)
+      })
 
     default:
       return state
