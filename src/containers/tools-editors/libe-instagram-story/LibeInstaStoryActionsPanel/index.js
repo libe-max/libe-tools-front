f6939857105bb70896a38f4c2051a43dc8832ae7
Maxime Fabas <maxime.fabas@gmail.com>
2018-11-05 21:50:22 +0100

    Tools – Libe insta story – Export button has an 'exporting' state

--- src/containers/tools-editors/libe-instagram-story/LibeInstaStoryActionsPanel/index.js
+++ src/containers/tools-editors/libe-instagram-story/LibeInstaStoryActionsPanel/index.js
@@ -2,21 +2,67 @@ import React, { Component } from 'react'
 import download from 'downloadjs'
 
 import Button from '../../../../components/buttons/Button'
+import Paragraph from '../../../../components/text-levels/Paragraph'
+
 
 import Wrapper from './style'
 
-export default class LibeInstaStorySavedActionsPanel extends Component {
+export default class LibeInstaStoryActionsPanel extends Component {
+  constructor () {
+    super()
+    this.state = { exporting: false }
+    this.rootClass = 'libe-insta-story-settings'
+    this.handleClick = this.handleClick.bind(this)
+  }
+
   render () {
-    const { props } = this
-    const { id } = props
-    const handleClick = e => fetch(`/api/build/${id}`)
-      .then(r => r.blob())
-      .then(blob => download(blob, 'story.zip'))
-      .catch(e => console.log(e))
-    return <Wrapper>
-      <Button
-        primary
-        onClick={handleClick}>Exporter</Button>
+    const { props, state, handleClick } = this
+    const { unsavedSettings } = props
+
+    /* Assign classes to component */
+    const r = this.rootClass
+    const classes = [r]
+    if (state.exporting) classes.push(`${r}_exporting`)
+
+    /* Display */
+    return <Wrapper
+      className={classes.join(' ')}>
+      <div className={`${r}__export-button`}>
+        <Button
+          minor
+          primary
+          onClick={handleClick}
+          disabled={unsavedSettings
+            ? 'Enregistrez les modifications'
+            : false}>
+          Exporter
+        </Button>
+      </div>
+      <div className={`${r}__exporting-message`}>
+        <Paragraph light small>Export en cours</Paragraph>
+        <img src='/images/loader.gif' />
+      </div>
     </Wrapper>
+  }
+
+  handleClick (e) {
+    const { props: {
+      id, pushNotification
+    } } = this
+    this.setState({ exporting: true })
+    return fetch(`/api/build/${id}`)
+      .then(r => r.json())
+      .then(json => {
+        if (json.err) throw new Error(json.err)
+        else fetch(json.data)
+          .then(r => r.blob())
+          .then(blob => download(blob, 'story.zip'))
+          .catch(e => console.log(e))
+      })
+      .catch(e => pushNotification(
+        `Une erreur est survenue lors de l'export. Le serveur a répondu : ${e.message}`,
+        'error'
+      ))
+      .finally(() => this.setState({ exporting: false }))
   }
 }
--- src/containers/tools-editors/libe-instagram-story/LibeInstaStoryActionsPanel/style.js
+++ src/containers/tools-editors/libe-instagram-story/LibeInstaStoryActionsPanel/style.js
@@ -1,3 +1,28 @@
 import styled from 'styled-components'
 
-export default styled.div``
+const r = '.libe-insta-story-settings'
+
+export default styled.div`
+display: flex;
+align-items: center;
+
+${r}__exporting-message {
+  align-items: center;
+  img {
+    height: ${p => p.theme.units(1)};
+    margin-left: ${p => p.theme.units(2)};
+    margin-top: ${p => p.theme.units(1.1)};
+    margin-bottom: ${p => p.theme.units(0.9)};
+  }
+}
+
+/* States */
+
+${r}__exporting-message {
+  display: none;
+}
+
+&${r}_exporting {
+  ${r}__export-button { display: none; }
+  ${r}__exporting-message { display: flex; }
+}`
